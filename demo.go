9df73deb7c283a1db6bfb9485d88cefe09908e83
xuyundong <xuyundong@douban.com>
2018-10-10 08:57:24 +0800

    init

---
+++ demo.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bwangelme/testmod"
+)
+
+func main() {
+	fmt.Println(testmod.Hi("xff"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bwangelme/testmod_demo
+
+require github.com/bwangelme/testmod v1.0.0
